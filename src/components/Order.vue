e4295db1f6a165605ae3de5b7f4764aaa3969f15
AlanCorn_02 <1561206831@qq.com>
2022-07-07 16:22:50 +0800

    :tada:src

---
+++ src/components/Order.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center flex-col">
+    <button class="btn btn-xs my-1 mx-1 btn-md w-48">立即预约</button>
+    <button class="btn btn-xs my-1 mx-1 btn-md w-48">预约历史</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Order"
+}
+</script>
+
+<style scoped>
+
+</style>
