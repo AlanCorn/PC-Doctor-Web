1410cf976ba23609be5b3178ec25dbdece61e804
AlanCorn_02 <1561206831@qq.com>
2022-07-08 14:09:19 +0800

    :gear:信息卡片组件

---
+++ src/components/OrderCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="shadow-md my-2 mx-1 card bg-base-200 shadow-xl">
+    <div class="flex flex-row card-body ">
+      <div class="w-72">
+        <div>问题描述：{{time}}</div>
+        <div>类别：{{time}}</div>
+        <div>预约时间：{{time}}</div>
+        <div>预约地点：{{time}}</div>
+        <div>状态：{{time}}</div>
+      </div>
+      <!-- 堆叠图片,靠右对齐 -->
+      <div class="w-36 stack ml-auto">
+        <img class="mask mask-square shadow-md rounded" src="https://placeimg.com/160/160/arch" />
+        <img class="mask mask-square shadow rounded" src="https://placeimg.com/160/160/arch" />
+        <img class="mask mask-square shadow rounded" src="https://placeimg.com/160/160/arch" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
