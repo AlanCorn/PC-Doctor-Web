5eb19b894c5b8fdf118eb60aaf86874dd1a7ee96
AlanCorn_02 <1561206831@qq.com>
2022-07-08 14:05:17 +0800

    :zap:路由更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
-// 注意： 在vite脚手架中, 引入时忽略.vue扩展名将导致错误
+// @@@ 注意： 在vite脚手架中, 引入时忽略.vue扩展名将导致错误
 
 const routes = [
     {
@@ -16,20 +16,39 @@ const routes = [
                 // 预约页面
                 path: '/order',
                 name: 'Order',
-                component: () => import('../components/Order.vue'),
+                component: () => import('../views/Order.vue'),
             },
             {
-                //
-                path: '/order',
-                name: 'Order',
-                component: () => import('../components/Order.vue'),
+                // 文档页面
+                path: '/document',
+                name: 'Document',
+                component: () => import('../views/Document.vue'),
             },
+            {
+                // 关于我们
+                path: '/about',
+                name: 'About',
+                component:() => import('../views/AboutUs.vue')
+            },
+            {
+                // 意见
+                path: '/suggestion',
+                name: 'Suggestion',
+                component:() => import('../views/Suggestion.vue')
+            },
+            {
+                // 帮助
+                path: '/help',
+                name: 'Help',
+                component:() => import('../views/Help.vue')
+            }
         ]
     }
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    // history: createWebHashHistory(),     // url中带井号
+    history: createWebHistory(),            // url中不带井号
     routes
 })
 
