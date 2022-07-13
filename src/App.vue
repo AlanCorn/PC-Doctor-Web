a01d94538dec6675b61232b75366e5938caf2ee4
AlanCorn_02 <1561206831@qq.com>
2022-07-13 20:53:31 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,45 @@
 
 <template>
   <router-view></router-view>
+  <notifications position="top center" classes="customNotify"/>
 </template>
 
 <style lang="scss">
+.customNotify {
+  // styling
+  margin: 10px 5px 0 5px;
+  padding: 10px;
+  font-size: 12px;
+  color: #ffffff;
+  // style for title line
+  .notification-title {
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  // style for content
+  .notification-content {
+    font-size: 15px;
+  }
+  // default (blue)
+  background: #44a4fc;
+  border-left: 5px solid #187fe7;
+  border-radius: 0.25rem;
+
+  // types (green, amber, red)
+  &.success {
+    background: #68cd86;
+    border-left-color: #42a85f;
+  }
+
+  &.warn {
+    background: #ffb648;
+    border-left-color: #f48a06;
+  }
+
+  &.error {
+    background: #e54d42;
+    border-left-color: #b82e24;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from "./router";
-import store from "./store";
-import './index.css'
+import router from "./router";      // vue-router
+import store from "./store";        // vuex
+import './index.css'                // tailwind 全局样式表
+// ElementPlus组件库
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import Notifications from '@kyvg/vue3-notification'
 
-
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(ElementPlus)
+    .use(store)
+    .use(router)
+    .use(Notifications)
+    .mount('#app')
