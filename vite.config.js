4cfbda48754e3dd792f85f6aedbdb15c6ea21981
AlanCorn_02 <1561206831@qq.com>
2022-07-22 22:52:22 +0800

    修复了生产环境错误

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // 关键代码
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,8 +18,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  define: {
-    'process.env': {}
-  }
-
 })
