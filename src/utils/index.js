cce2820a79687ae31960969b9c0873571034f2f9
AlanCorn_02 <1561206831@qq.com>
2022-07-26 22:30:51 +0800

    vite下请求静态资源方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,11 +3,16 @@ import moment from 'moment';
 // function pushRouter(path){
 //     router.push(path)
 //
-function timeFormatter(datetime){
+const timeFormatter = (datetime) => {
     return moment(datetime).format('YYYY-MM-DD HH:mm:ss')
+}
+// Vite 下的静态资源获取方法(webpack的require无法使用)
+const getImageUrl = (fileName) => {
+    return new URL(`../assets/image/vite-images/${fileName}`, import.meta.url).href
 }
 
 
 export {
-    timeFormatter
+    timeFormatter,
+    getImageUrl
 }
