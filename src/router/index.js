c339ff9f5671b0daedcfc6c3e023f61605b020c1
AlanCorn_02 <1561206831@qq.com>
2022-07-27 23:20:25 +0800

    每个路由现在都有独特的名字了

--- src/router/index.js
+++ src/router/index.js
@@ -16,42 +16,49 @@ const routes = [
                 // 预约页面
                 path: '/order',
                 name: 'Order',
+                meta: { title: "电脑医院-首页" },
                 component: () => import('../views/Order/Order.vue'),
             },
             {
                 // 预约页面
                 path: '/addOrder',
                 name: 'AddOrder',
+                meta: { title: "电脑医院-添加预约" },
                 component: () => import('../views/Order/OrderAdd.vue'),
             },
             {
                 // 预约页面
                 path: '/orderInfo',
                 name: 'OrderInfo',
+                meta: { title: "电脑医院-预约信息" },
                 component: () => import('../views/Order/OrderInfo.vue'),
             },
             {
                 // 文档页面
                 path: '/document',
                 name: 'Document',
+                meta: { title: "电脑医院-文档" },
                 component: () => import('../views/Document.vue'),
             },
             {
                 // 关于我们
                 path: '/about',
                 name: 'About',
+                meta: { title: "电脑医院-关于我们" },
                 component:() => import('../views/AboutUs.vue')
             },
             {
                 // 意见
                 path: '/suggestion',
                 name: 'Suggestion',
+                meta: { title: "电脑医院-意见" },
                 component:() => import('../views/Suggestion.vue')
             },
             {
                 // 帮助
                 path: '/help',
                 name: 'Help',
+                meta: { title: "电脑医院-帮助" },
                 component:() => import('../views/Help.vue')
             }
         ]
@@ -65,8 +72,13 @@ const router = createRouter({
 })
 
 //todo 全局路由守卫，内部做一些登录验证处理
-router.beforeEach((to,from) =>{
-
+router.beforeEach((to, from, next) => {//beforeEach是router的钩子函数，在进入路由前执行
+    //添加标题
+    if (to.meta.title) {
+        document.title = to.meta.title
+    }
+    next()
 })
+
 
 export default router
