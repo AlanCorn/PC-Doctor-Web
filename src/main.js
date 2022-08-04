c73904dff92b0c3888fdbc02273df9a32dc955a0
AlanCorn_02 <1561206831@qq.com>
2022-08-04 21:01:48 +0800

    使用animate.css插件

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import './index.css'                // tailwind 全局样式表
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import Notifications from '@kyvg/vue3-notification'
+// 一个css动画库
+import animate from 'animate.css'
 
 createApp(App)
     .use(ElementPlus)
+    .use(animate)
     .use(store)
     .use(router)
     .use(Notifications)
