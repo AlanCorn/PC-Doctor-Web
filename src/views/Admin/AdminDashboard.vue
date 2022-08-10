ddf0f86d151648be7cab247cf3ad23d888f2678d
AlanCorn_02 <1561206831@qq.com>
2022-08-11 01:00:42 +0800

    管理员页面主页功能增加

--- src/views/Admin/AdminDashboard.vue
+++ src/views/Admin/AdminDashboard.vue
@@ -1,15 +1,50 @@
 <template>
-	<div class="container p-10">
-		<div class="text-primary text-4xl font-bold">我的名片</div>
-    <DoctorCard/>
-		<div class="text-primary text-4xl font-bold">待接单</div>
-		<div class="text-primary text-4xl font-bold">正在处理</div>
-		<div class="text-primary text-4xl font-bold">已完成</div>
-	</div>
+  <div class="flex items-center flex-col">
+    <div class="w-11/12 py-10">
+      <div class="text-primary text-4xl font-bold">我的名片</div>
+      <DoctorCard/>
+      <OrderCardPages
+          v-for="(each,index) in orderCardPagesData.titles"
+          :key="index"
+          :title="each"
+          :list="adminOrderList(each).value"
+      />
+    </div>
+  </div>
+
 </template>
 
 <script setup>
 import DoctorCard from "../../components/DoctorCard.vue";
+import OrderCardPages from '../../components/OrderCardPages.vue'
+import {computed, onBeforeMount, reactive} from "vue";
+import { useStore } from "vuex";
+
+
+const store = useStore()
+
+const adminOrderList = (type) => computed(() => store.getters.adminOrderListGetter(type))
+
+// 列表数据
+const orderCardPagesData = reactive({
+  titles: ['正在处理','待接单','已完成'],
+})
+// 筛选与查询参数
+const orderCardPagesParams = reactive({
+  filterType:['setProcessingList','setWaitingList','setFinishedList'],
+  filterParams: [{
+    page:1,
+    status:1,
+    doctor_id:computed(() => store.state.user.user_id)
+  },{
+    page:1,
+    status:0,
+  },{
+    page:1,
+    status:2,
+    doctor_id:computed(() => store.state.user.user_id)
+  }],
+})
 
 const infoForm = {
 	avatar: "https://s.gravatar.com/avatar/059edf8a64b449e8ad399de39c3309aa?s=80",
@@ -20,4 +55,12 @@ const infoForm = {
 	score: "89", // 评分
 };
 
+onBeforeMount(() => {
+  orderCardPagesParams.filterType.forEach((each,index) => {
+    store.dispatch('getAdminOrderList',{
+      params:orderCardPagesParams.filterParams[index],
+      type:each
+    })
+  })
+})
 </script>
