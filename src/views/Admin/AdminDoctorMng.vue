bc57e75c39e9d5b0f5f9af4d0d9fc11c5dc2a022
AlanCorn_02 <1561206831@qq.com>
2022-08-11 01:00:53 +0800

    电医管理页面

--- src/views/Admin/AdminDoctorMng.vue
+++ src/views/Admin/AdminDoctorMng.vue
@@ -1,11 +1,31 @@
 <template>
-  <DoctorCard/>
-  <DoctorCard/>
-  <DoctorCard/>
+  <div class="flex items-center flex-col">
+    <div class="w-11/12 pt-10">
+    <div class="text-primary text-4xl font-bold">电医管理</div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
+      <DoctorCard v-for="index in 10" key="index" class="m-2"/>
+    </div>
+    <div class="btn-group">
+      <button class="btn btn-sm">1</button>
+      <button class="btn btn-sm btn-active">2</button>
+      <button class="btn btn-sm">3</button>
+      <button class="btn btn-sm">4</button>
+    </div>
+    </div>
+  </div>
+
 </template>
 
 <script setup>
 import DoctorCard from "../../components/DoctorCard.vue";
 
-
+const infoForm = {
+  avatar: "https://s.gravatar.com/avatar/059edf8a64b449e8ad399de39c3309aa?s=80",
+  name: "杨志文",
+  sex: "男",
+  contact: "QQ:1561206831",
+  grade: "电医", // 职位
+  score: "89", // 评分
+};
 </script>
