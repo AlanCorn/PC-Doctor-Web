2499950006b29ad9b75c85945aa9a33e15322356
AlanCorn_02 <1561206831@qq.com>
2022-08-13 22:39:21 +0800

    电医卡片组件进一步完善

--- src/components/DoctorCard.vue
+++ src/components/DoctorCard.vue
@@ -1,68 +1,89 @@
 <template>
-		<div
-			class="card border bg-white shadow-md px-3 py-2 my-3 max-w-md flex-row gap-3"
-		>
-			<div class="avatar flex-col justify-center">
-				<div class="w-20 h-20 lg:w-24 lg:h-24 rounded-full">
-					<img :src="infoForm.avatar" />
-				</div>
-			</div>
-			<div class="flex flex-col justify-center">
-				<div class="flex">
-					<div class="inline font-bold mr-1">
-						{{ infoForm.name }}
-					</div>
-					<SexIcon :sex="infoForm.sex" />
-				</div>
-				<div>
-					{{ infoForm.contact }}
-				</div>
-				<div class="rating rating-sm">
-					<input
-						type="radio"
-						name="rating-6"
-						class="mask mask-star-2 bg-orange-400"
-					/>
-					<input
-						type="radio"
-						name="rating-6"
-						class="mask mask-star-2 bg-orange-400"
-						checked
-					/>
-					<input
-						type="radio"
-						name="rating-6"
-						class="mask mask-star-2 bg-orange-400"
-					/>
-					<input
-						type="radio"
-						name="rating-6"
-						class="mask mask-star-2 bg-orange-400"
-					/>
-					<input
-						type="radio"
-						name="rating-6"
-						class="mask mask-star-2 bg-orange-400"
-					/>
-				</div>
-			</div>
+  <div
+      class="card border bg-white shadow-md px-3 py-2 my-3 max-w-md flex-row gap-3"
+  >
+    <MyAvatar :imgSrc="props.doctorInfo.avatar" :name="props.doctorInfo.user_name"/>
+    <div class="flex flex-col justify-center">
+      <div class="flex">
+        <div class="inline font-bold mr-1">
+          {{ props.doctorInfo.user_name }}
+        </div>
+        <SexIcon sex="男"/>
+      </div>
+      <div>
+        {{ props.doctorInfo.contact_details }}
+      </div>
+      <!--				<div class="rating rating-sm">-->
+      <!--					<input-->
+      <!--						type="radio"-->
+      <!--						name="rating-6"-->
+      <!--						class="mask mask-star-2 bg-orange-400"-->
+      <!--					/>-->
+      <!--					<input-->
+      <!--						type="radio"-->
+      <!--						name="rating-6"-->
+      <!--						class="mask mask-star-2 bg-orange-400"-->
+      <!--						checked-->
+      <!--					/>-->
+      <!--					<input-->
+      <!--						type="radio"-->
+      <!--						name="rating-6"-->
+      <!--						class="mask mask-star-2 bg-orange-400"-->
+      <!--					/>-->
+      <!--					<input-->
+      <!--						type="radio"-->
+      <!--						name="rating-6"-->
+      <!--						class="mask mask-star-2 bg-orange-400"-->
+      <!--					/>-->
+      <!--					<input-->
+      <!--						type="radio"-->
+      <!--						name="rating-6"-->
+      <!--						class="mask mask-star-2 bg-orange-400"-->
+      <!--					/>-->
+      <!--				</div>-->
+    </div>
 
-			<div class="absolute top-2 right-3 badge">
-				{{ infoForm.grade }}
-			</div>
-		</div>
+    <div class="absolute top-2 right-3 badge">
+      {{ grade }}
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import {computed, reactive} from "vue";
 import SexIcon from "./SexIcon.vue";
+import MyAvatar from "./MyAvatar.vue";
 
-const infoForm = {
-	avatar: "https://s.gravatar.com/avatar/059edf8a64b449e8ad399de39c3309aa?s=80",
-	name: "杨志文",
-	sex: "男",
-	contact: "QQ:1561206831",
-	grade: "电医", // 职位
-	score: "89", // 评分
-};
+const props = defineProps({
+  doctorInfo:{
+    type:Object,
+    default:{
+      user_name:'无',
+      sex: "男",
+      level:'0',
+      contact_details:'QQ:xxx'
+    }
+  }
+})
+
+console.log('DoctorCard',props)
+const grade = computed(() => {
+  switch (props.doctorInfo.level){
+    case '0':
+      return '普通用户'
+    case '1':
+      return '电医'
+    case '2':
+      return '管理员'
+  }
+})
+
+// const infoForm = {
+//   // avatar: "https://s.gravatar.com/avatar/059edf8a64b449e8ad399de39c3309aa?s=80",
+//   name: "杨志文",
+//   sex: "男",
+//   contact: "QQ:1561206831",
+//   grade: "电医", // 职位
+//   score: "89", // 评分
+// };
 </script>
