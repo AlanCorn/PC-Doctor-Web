825b62e9d9d09724be69c670e884c41814fdf9a4
AlanCorn_02 <1561206831@qq.com>
2022-08-13 22:38:40 +0800

    无关紧要的修改

--- src/views/Suggestion.vue
+++ src/views/Suggestion.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="flex flex-col justify-center items-center min-h-screen">
-    <div class="text-5xl">todo:意见</div>
+  <div class="flex items-center flex-col">
+    <div class="hero min-h-screen">
+      <div class="hero-overlay bg-opacity-10 gradient-title">意见</div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +13,11 @@ export default {
 </script>
 
 <style scoped>
-
+.gradient-title {
+  font-size: 72px;
+  background: rgb(238,174,202);
+  background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,195,233,1) 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
 </style>
