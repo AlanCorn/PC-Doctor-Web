0ce81992c33b4a59ad9be1b3d89d9befff5dc653
AlanCorn_02 <1561206831@qq.com>
2022-08-16 22:26:45 +0800

    分类标签组件化

---
+++ src/components/CateTabs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tabs tabs-boxed bg-base-100 text-2xl items-start mt-5">
+    <a v-for="(each,index) in cateList"
+       :key="index"
+       :class="{'tab transition duration-500 rounded-md':true,'tab-active':index === activeCateIndex} "
+       @click="(activeCateIndex = index) && onChangeState(index)">
+      {{ each }}
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  cateList:Array,
+  onChangeState:Function
+})
+
+const activeCateIndex = ref(0)
+</script>
+
+<style lang='scss' scoped>
+
+</style>
