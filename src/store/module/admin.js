446a36cc0ea76e1112a59607bcd9d1b1499fcba3
AlanCorn_02 <1561206831@qq.com>
2022-08-23 18:37:57 +0800

    新增userList state

--- src/store/module/admin.js
+++ src/store/module/admin.js
@@ -1,25 +1,30 @@
 import userApi from '../../api/userApi'
 import {notify} from "@kyvg/vue3-notification";
-import user from "../../api/userApi";
+import adminApi from "../../api/adminApi";
 
 const admin = {
     state: () => ({
         // 电医在管理员主页所看到的orderList
-        adminOrderList:{
+        adminOrderList: {
             // 正在处理的预约
-            processingList:[],
-            processingListSize:0,
+            processingList: [],
+            processingListSize: 0,
             // 排队中的的预约
-            waitingList:[],
-            waitingListSize:0,
+            waitingList: [],
+            waitingListSize: 0,
             // 已完成的预约
-            finishedList:[],
-            finishedListSize:0,
+            finishedList: [],
+            finishedListSize: 0,
+        },
+        // 用户管理下的列表
+        userList: {
+            list: [],
+            size: 0
         }
     }),
     getters: {
         adminOrderListGetter: (state) => (type) => {
-            switch (type){
+            switch (type) {
                 case '正在处理':
                     return state.adminOrderList.processingList
                 case '待接单':
@@ -30,33 +35,53 @@ const admin = {
         }
     },
     mutations: {
-        setProcessingList(state, data){
+        setProcessingList(state, data) {
             state.adminOrderList.processingList = data.list
             state.adminOrderList.processingListSize = data.size
         },
-        setWaitingList(state, data){
+        setWaitingList(state, data) {
             state.adminOrderList.waitingList = data.list
             state.adminOrderList.waitingListSize = data.size
         },
-        setFinishedList(state, data){
+        setFinishedList(state, data) {
             state.adminOrderList.finishedList = data.list
             state.adminOrderList.finishedListSize = data.size
         },
+        setUserList(state, data) {
+            state.userList.list = data.list
+            state.userList.size = data.size
+        }
     },
     actions: {
         // 参数说明: @params=发送/query请求的参数     @listType=对应三个列表的mutations
         // 获取电医管理的三个列表
-        getAdminOrderList(content,data){
+        getAdminOrderList(content, data) {
             // 使用user Api 发送异步请求，提交commit
-            user.getOrderHistory(data.params).then(res =>{
-                content.commit(data.type, {
-                    list : res.data.appointment_list,
-                    size : res.data.size
-                })
-            }).catch(err =>{
+            userApi.getOrderHistory(data.params).then(res => {
+                if (res.data.code === 0){
+                    content.commit(data.type, {
+                        list: res.data.appointment_list,
+                        size: res.data.size
+                    })
+                }
+            }).catch(err => {
                 console.log(err)
             })
         },
+        // 获取用户列表，管理员(2)进行用户管理
+        getUserList(content, params) {
+            adminApi.getUserList(params).then(res => {
+                console.log(res.data.user_list)
+                if (res.data.code === 0){
+                    content.commit('setUserList', {
+                        list: res.data.user_list,
+                        size: res.data.size
+                    })
+                }
+            }).catch(err => {
+                console.log(err)
+            })
+        }
     }
 }
 
