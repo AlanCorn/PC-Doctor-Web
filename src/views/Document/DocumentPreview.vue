2d00323c99f0f1459435ee4819ae5f89ce0ee1c5
AlanCorn_02 <1561206831@qq.com>
2022-09-02 00:01:55 +0800

    文档预览界面与后端对接

--- src/views/Document/DocumentPreview.vue
+++ src/views/Document/DocumentPreview.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center">
     <div class="card w-11/12 md:w-3/4 lg:w-2/3 my-10">
-      <div class="absolute top-5 right-5">
+      <div v-if='isAdmin' class="absolute top-5 right-5">
         <button class="btn btn-primary btn-sm" @click="handleEdit">
           编辑
         </button>
@@ -12,40 +12,20 @@
 </template>
 
 <script setup>
-import {ref} from "vue";
-import {useRouter} from "vue-router";
+import {computed} from "vue";
+import {useRoute, useRouter} from "vue-router";
+import {useStore} from "vuex";
 
+const route = useRoute()
 const router = useRouter()
-// 预览文本
-const text = ref('# **电医预约**\n' +
-    '> 这里是电医预约的文档页面\n' +
-    '\n' +
-    '## 预览\n' +
-    '### 链接\n' +
-    '[电医预约](http://116.62.129.37)\n' +
-    '### 图片预览\n' +
-    '![Description](https://w.wallhaven.cc/full/3z/wallhaven-3zygdy.png)\n' +
-    '\n' +
-    '### 这是一个表格\n' +
-    '\n' +
-    '|column1|column2|column3|\n' +
-    '|-|-|-|\n' +
-    '|content1|content2|content3| \n' +
-    '\n' +
-    '### 列表\n' +
-    '1. 有序列表\n' +
-    '\n' +
-    '- 无序列表\n' +
-    '\n' +
-    '### \n' +
-    '\n' +
-    '### 代码块\n' +
-    '```sudo rm -rf```\n' +
-    'or\n' +
-    '``` \n' +
-    'sudo rm -rf /\n' +
-    '```\n')
+const store = useStore()
 
+// 获取文章
+store.dispatch('queryDocById',route.query.id)
+
+const isAdmin = computed(() => store.state.user.level === '2')
+// 预览文本
+const text = computed(() => store.state.doc.nowDocText)
 
 function handleEdit() {
   router.push({ name: 'Edit', params: { text: text.value } })
