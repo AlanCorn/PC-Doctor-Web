2b04630075bca078b38686338defdf1d9fbcebdf
AlanCorn_02 <1561206831@qq.com>
2022-09-01 23:58:12 +0800

    新增文档的vuex状态

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import {createStore} from "vuex";
 import order from './module/order'
 import user from './module/user'
 import admin from './module/admin'
+import doc from './module/doc'
 
 const store = createStore({
     modules:{
         order,
         user,
-        admin
+        admin,
+        doc
     },
 })
 
---
+++ src/store/module/doc.js
@@ -0,0 +1,64 @@
+import {notify} from "@kyvg/vue3-notification";
+import userApi from "@/api/userApi";
+import fileApi from "@/api/file";
+
+const doc = {
+    state: () => ({
+        // 正在处理的文档
+        nowDoc:{},
+        // 正在处理的文档中的文本内容
+        nowDocText:'',
+        // 所有文档列表
+        docList:[]
+    }),
+    getters: {
+    },
+    mutations: {
+        setNowDoc(state, doc) {
+            state.nowDoc = doc
+        },
+        nowDocText(state, text) {
+            state.nowDocText = text
+        },
+        setDocList(state, list) {
+            state.docList = list
+        },
+    },
+    actions: {
+        // 根据id请求文档
+        queryDocById(content, id) {
+            // 使用user Api 发送异步请求，提交commit
+            console.log(id)
+            new Promise(resolve => {
+                userApi.queryDoc({
+                    id:id
+                }).then(res => {
+                    if (res.data.code === 0){
+                        content.commit('setNowDoc',res.data.doc_list[0])
+                        resolve(res.data.doc_list[0].file)
+                    }
+                }).catch(err => {
+                    console.log(err)
+                })
+            }).then(fileName => {
+                console.log(fileName)
+                fileApi.getPlainTextFile(fileName).then(res =>
+                    content.commit('nowDocText',res.data)
+                )
+            })
+        },
+        // 请求文档列表 默认无参数查询全部
+        queryDocList(content, params= {}) {
+            // 使用user Api 发送异步请求，提交commit
+            userApi.queryDoc(params).then(res => {
+                if (res.data.code === 0){
+                    content.commit('setDocList',res.data.doc_list)
+                }
+            }).catch(err => {
+                console.log(err)
+            })
+        },
+    }
+}
+
+export default doc
