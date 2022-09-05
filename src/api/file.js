f4efd58d4c593a8bc7aba203e0fdc490f28adf91
AlanCorn_02 <1561206831@qq.com>
2022-09-05 17:25:21 +0800

    上传接口修改，将图片压缩至合适大小

--- src/api/file.js
+++ src/api/file.js
@@ -1,24 +1,50 @@
 import request from './request'
 import FormData from 'form-data'
 import baseUrl from './urls'
+import {imageCompressor} from "@/utils";
 
 const headers = {
-    'Content-Type':'multipart/form-data'
+    'Content-Type': 'multipart/form-data'
 }
 
 
-// 文件上传APi @imgType = ['appointment','user_pic','doc_pic'] ;
 const fileApi = {
-    uploadFile(file,imgType = 'appointment'){
+    // 文件上传APi @fileType = {
+    //      'appointment':用户预约上传的图片,
+    //      'user_pic'：头像,
+    //      'doc_pic'：文档中使用的图片,
+    //      'doc'：文档markdown文件
+    // } ;
+    async uploadFile(file, fileType = 'appointment') {
+        console.log("是否进行压缩", ['appointment', 'user_pic', 'doc_pic'].indexOf(fileType) !== -1)
+        console.log("压缩前文件", file)
+
         const form = new FormData();
-        form.append('file',file)
-        form.append('type',imgType)
+        form.append('type', fileType)
+        if (fileType === 'doc') {
+            form.append('file', file)
+        } else if (['appointment', 'doc_pic'].indexOf(fileType) !== -1) {
+            await imageCompressor(file).then(res => {
+                form.append('file', res)
+                console.log(`${fileType}压缩后文件`, res)
+            })
+        } else if (fileType === 'user_pic') {
+            await imageCompressor(file, {
+                size: 300,
+                type: "image/jpeg",
+                width: 300,
+                height: 300,
+            }).then(res => {
+                form.append('file', res)
+                console.log(`${fileType}压缩后文件`, res)
+            })
+        }
         return request.post("/upload", form, {
             headers
         })
     },
-    getPlainTextFile(fileName){
-        return request.get(`${ baseUrl.testUrl }file/doc/${ fileName }`)
+    getPlainTextFile(fileName) {
+        return request.get(`${baseUrl.testUrl}file/doc/${fileName}`)
     },
 }
 
