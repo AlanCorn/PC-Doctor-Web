db98b58a039d2d31ed18f1e24c3a1e95e1fc7d9c
AlanCorn_02 <1561206831@qq.com>
2022-09-06 10:43:54 +0800

    文件上传接口修改

--- src/api/file.js
+++ src/api/file.js
@@ -9,15 +9,16 @@ const headers = {
 
 
 const fileApi = {
-    // 文件上传APi @fileType = {
-    //      'appointment':用户预约上传的图片,
-    //      'user_pic'：头像,
-    //      'doc_pic'：文档中使用的图片,
-    //      'doc'：文档markdown文件
-    // } ;
+    /**  @params fileType = {
+         'appointment':用户预约上传的图片,
+         'user_pic'：头像,
+         'doc_pic'：文档中使用的图片,
+         'doc'：文档markdown文件
+    } ;
+     */
     async uploadFile(file, fileType = 'appointment') {
-        console.log("是否进行压缩", ['appointment', 'user_pic', 'doc_pic'].indexOf(fileType) !== -1)
-        console.log("压缩前文件", file)
+        // console.log("是否进行压缩", ['appointment', 'user_pic', 'doc_pic'].indexOf(fileType) !== -1)
+        // console.log("压缩前文件", file)
 
         const form = new FormData();
         form.append('type', fileType)
@@ -26,17 +27,17 @@ const fileApi = {
         } else if (['appointment', 'doc_pic'].indexOf(fileType) !== -1) {
             await imageCompressor(file).then(res => {
                 form.append('file', res)
-                console.log(`${fileType}压缩后文件`, res)
+                // console.log(`${fileType}压缩后文件`, res)
             })
         } else if (fileType === 'user_pic') {
             await imageCompressor(file, {
-                size: 300,
+                size: 50,
                 type: "image/jpeg",
                 width: 300,
                 height: 300,
             }).then(res => {
                 form.append('file', res)
-                console.log(`${fileType}压缩后文件`, res)
+                // console.log(`${fileType}压缩后文件`, res)
             })
         }
         return request.post("/upload", form, {
