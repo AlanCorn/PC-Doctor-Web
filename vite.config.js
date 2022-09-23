97ea054c747f2472fbc03db9eba6ab61a105ddcc
AlanCorn_02 <1561206831@qq.com>
2022-09-23 17:51:38 +0800

    sass与tailwind公共样式目录结构优化

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: '@import "./src/assets/scss/var.scss";'
+                additionalData: '@import "./src/style/scss/var.scss";'
             }
         }
     },
